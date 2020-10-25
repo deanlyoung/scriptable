c9281e5baa3b98ef0c0e29cb593d617ce2cc1ca0
Dean L. Young <deanlyoung@users.noreply.github.com>
2020-10-24 18:26:23 -0700

    remove countdown

--- Climate Clock.js
+++ Climate Clock.js
@@ -1,8 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-green; icon-glyph: clock;
-let countdown = importModule('countdown')
-
+// let countdown = importModule('countdown')
 
 // instantiate widget
 const data = await fetchData()
